ba7040e6990dced8275b17b48f56821fba9a57e7
Kyoko-Sakamoto759 <sakamo.1992@gmail.com>
2025-09-16 20:09:16 +0900

    pythonの課題

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # HTML_Test_App
+
+#きょん@59期のハッカソン事前テストのpythonの課題の動画です。
+#ご確認よろしくお願いします。
+
+https://www.loom.com/share/db605132910b44e68dac08aa2500d572?sid=911a1da3-13ac-4fe7-bc59-324347d62cea
---
+++ example.py
@@ -0,0 +1,25 @@
+#pythonの課題
+
+# 1
+#print('hello world')
+
+# 2
+def greet():
+    print('こんにちは')
+
+# 3
+def print_name(name):
+    print(f'私の名前は{name}です')
+
+'''
+# 4
+def get_greet():
+    return('おはようございます')
+
+x=get_greet()
+print(x)
+'''
+
+# 5
+def add(a, b):
+    print(a + b)
